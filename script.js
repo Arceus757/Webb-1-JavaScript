4404e6bc7dbed03a26fe51c272821ee8e80d5bea
Dmytro <dmytro.malanchuk@elev.ga.ntig.se>
2023-12-13 09:41:01 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+console.log("Good Evening");
+
+window.alert("World")
+
+function doSomething(){
+    document.getElementById("test").innerHTML = "GoodBye"
+    document.getElementById("test").style.backgroundColor = "blue"
+}
